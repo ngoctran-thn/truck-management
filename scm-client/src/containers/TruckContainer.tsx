508489d0aa38e741a5185a78195fa7218adbd075
Ngoc Tran <tranthainhien1603@gmail.com>
2022-11-01 22:46:04 +0200

    add react rout so each truck has a separate URL

--- scm-client/src/containers/TruckContainer.tsx
+++ scm-client/src/containers/TruckContainer.tsx
@@ -2,12 +2,14 @@ import { useState,useEffect, FC} from "react";
 import TruckDatagridComponent from "../components/TruckDatagridComponent";
 import { TruckDetail } from "../components/TruckDetail";
 
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
 const TruckContainer:FC=props=>{
     return(
         <div>
             <h1>SCM Application</h1>
-            <TruckDatagridComponent/>
-            {/* <TruckDetail/> */}
+            <Outlet/>
+           
         </div>
     )
 }
